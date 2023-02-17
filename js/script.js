ee2c7501b0f814062a326cdcc9168b0ad60a91ac
Kemyl Fernandez <kemylfernandez299@gmail.com>
2023-02-17 01:04:26 -0400

    FINAL

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-async function getContacts() {
-    const data = await fetch("http://www.raydelto.org/agenda.php").then(function (result) {
-        return result.json();
-    }).then(function (result) {
-        for(let i of result) {
-            const contact = i;
-            const lista = document.getElementById("contacts");
-            lista.appendChild(addContactsToBody(contact));
-        }
-    });
-}
-
-console.log(getContacts());
-
 function addContactsToBody(contact) {
     const nDiv = document.createElement("div");
     nDiv.className = "contacts";
@@ -19,16 +5,36 @@ function addContactsToBody(contact) {
     return nDiv;
 }
 
+function getContacts() {
+    const data = fetch("http://www.raydelto.org/agenda.php").then(function (result) {
+        return result.json();
+    }).then(async function (result) {
+        for(let i of result) {
+            const contact = i;
+            const lista = document.getElementById("contacts");
+            lista.appendChild(addContactsToBody(await contact));
+        }
+    });
+}
+
 function formToJSON(form) {
     const data = new FormData(form);
     const value = Object.fromEntries(data.entries());
     const obj = JSON.stringify(value);
-    document.getElementById("form").reset();
-    console.log(obj);
     return obj;
 }
 
 function enviarJSON(form) {
-    formToJSON(form);
+    let check = document.forms["form"]["nombre"].value;
+    if (check == "") {
+        alert("Llene los campos del formulario");
+    } else {
+        console.log(formToJSON(form));
+        fetch("http://www.raydelto.org/agenda.php", {
+            method: 'POST',
+            body: formToJSON(form),
+        });
+    }
+    document.getElementById("form").reset();
 }
 
